332347b1e4b4fc5403793117d1befe0315d564d2
Ramyatha <ramyathaaa@gmail.com>
2025-02-26 16:37:55 +0530

    Added requirements.txt and prepared for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,11 +34,13 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '356f-2401-4900-1c26-65f5-19ff-f806-9a9e-3867.ngrok-free.app'
+    '356f-2401-4900-1c26-65f5-19ff-f806-9a9e-3867.ngrok-free.app',
+    'digipattanam-1.onrender.com',
 ]
 
 CSRF_TRUSTED_ORIGINS=[
     'https://356f-2401-4900-1c26-65f5-19ff-f806-9a9e-3867.ngrok-free.app',
+    'https://digipattanam-1.onrender.com',
 ]
 
 
@@ -150,7 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 PAYU_PAYOUTS = {
     "API_KEY": "75DMsQ",  # Your PayU API Key
-    "SALT": "your_salt_here",
+    "SALT": "",
     "CLIENT_ID": "702f87fed2e6b5f0316de199901d56043abec58c917af25b437154cc331f75df",
     "CLIENT_SECRET": "65258bfdf5c5fff37c01b0bf6f8243051b56139cd40cf3cc9c317f3323f98f6d",
     "BASE_URL": "https://uat-api.payu.in",  # Use production URL in live mode
