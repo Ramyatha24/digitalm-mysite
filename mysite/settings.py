8c112ca880bcbda3880ea904cbb728f4f7518680
Ramyatha <ramyathaaa@gmail.com>
2025-03-02 14:00:14 +0530

    Re-added settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,7 +146,7 @@ import os
 # STATIC FILES
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 # MEDIA FILES
@@ -211,12 +211,7 @@ DATABASES = {
     }
 }
 
-# Static & Media files
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
