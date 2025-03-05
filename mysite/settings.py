2faf7ac9869487218e9e04a228c95151a5dd3396
Ramyatha <ramyathaaa@gmail.com>
2025-03-05 18:33:21 +0530

    changed in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -181,7 +181,7 @@ DEBUG = env.bool("DEBUG", default=False)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=["localhost"])
-CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=["https://digitalm-mysite.onrender.com"])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=["https://digitalm-mysite.vercel.app"])
 
 
 
